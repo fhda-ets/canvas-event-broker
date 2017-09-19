da871e5d203a385979a6b86a3216afcb6aebc178
Matt Rapczynski <rapczynskimatthew@fhda.edu>
2017-09-19 08:08:13 -0700

    Bug fix to SQL to prevent duplicater instructor records

--- src/sql/Instructors.sql
+++ src/sql/Instructors.sql
@@ -3,9 +3,7 @@ select distinct
     spriden_pidm as "pidm",
     nvl(spbpers_pref_first_name, spriden_first_name) as "firstName",
     spriden_last_name as "lastName",
-    (select lower(goremal_email_address) from goremal where rownum = 1 and goremal_pidm = spriden_pidm and goremal_status_ind in ('A') and goremal_emal_code = 'FHDA') as "email",
-    sirasgn_category as "category",
-    sirasgn_primary_ind as "primary"
+    (select lower(goremal_email_address) from goremal where rownum = 1 and goremal_pidm = spriden_pidm and goremal_status_ind in ('A') and goremal_emal_code = 'FHDA') as "email"
 from
     sirasgn,
     spriden,
@@ -16,6 +14,3 @@ where
     and spriden_pidm = sirasgn_pidm
     and spriden_change_ind is null
     and spbpers_pidm = spriden_pidm
-order by
-    sirasgn_category asc,
-    sirasgn_primary_ind asc
